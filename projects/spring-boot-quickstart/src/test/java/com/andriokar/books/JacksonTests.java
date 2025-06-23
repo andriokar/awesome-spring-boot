d667cd211054386a7dd6bede52c3148dc28f55d5
Andriy <karpus.andrii@proton.me>
2025-06-23 18:47:58 +0200

    JSON to Java object

--- projects/spring-boot-quickstart/src/test/java/com/andriokar/books/JacksonTests.java
+++ projects/spring-boot-quickstart/src/test/java/com/andriokar/books/JacksonTests.java
@@ -26,4 +26,24 @@ public class JacksonTests {
                         "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria"
                                 + " Montgomery\",\"year\":\"2005\"}");
     }
+
+    @Test
+    public void testThatObjectMapperCanCreateJavaObjectFromJsonObject()
+            throws JsonProcessingException {
+        Book book =
+                Book.builder()
+                        .isbn("978-0-13-478627-5")
+                        .title("The Enigma of Eternity")
+                        .author("Aria Montgomery")
+                        .yearPublished("2005")
+                        .build();
+        String json =
+                "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
+                        + " Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}";
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        Book result = objectMapper.readValue(json, Book.class);
+        assertThat(result).isEqualTo(book);
+    }
 }
