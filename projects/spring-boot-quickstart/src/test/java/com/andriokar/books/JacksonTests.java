d268140abd767a260fd6c21c1d1121645cf2a3f7
Andriy <karpus.andrii@proton.me>
2025-06-23 18:54:19 +0200

    Ingoring properties

--- projects/spring-boot-quickstart/src/test/java/com/andriokar/books/JacksonTests.java
+++ projects/spring-boot-quickstart/src/test/java/com/andriokar/books/JacksonTests.java
@@ -38,7 +38,7 @@ public class JacksonTests {
                         .yearPublished("2005")
                         .build();
         String json =
-                "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
+                "{\"foo\":\"bar\", \"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
                         + " Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}";
 
         final ObjectMapper objectMapper = new ObjectMapper();
